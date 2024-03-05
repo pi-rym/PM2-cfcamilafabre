1314516cea7fd582fb7be7fab039b0dea00cd7b3
cfcamilafabre <camilafabre98@gmail.com>
2024-03-05 09:49:02 -0300

    Estructura html, css, y scripts

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,34 @@
-console.log(tempData);
+const seccionPeliculas = document.querySelector(".seccion-peliculas");
+
+const newArray = tempData.map((pelicula) => {
+    const { title, year, director, duration, genre, rate, poster } = pelicula;
+
+    const tarjetaPelicula = document.createElement('div');
+    tarjetaPelicula.classList.add("pelicula");
+
+    const tituloPeli = document.createElement('h3');
+    tituloPeli.classList.add("titulo-pelicula");
+    tituloPeli.innerHTML = title;
+
+    const descripcionPeli = document.createElement('p');
+    descripcionPeli.classList.add("descripcion-peli");
+    descripcionPeli.innerText = `${year}, 
+    ${director}, 
+    ${duration},
+     ${genre}, 
+     ${rate}`;
+
+    const imagenPeli = document.createElement('img');
+    imagenPeli.classList.add("imagen-peli");
+    imagenPeli.src = poster;
+
+    tarjetaPelicula.appendChild(tituloPeli);
+    tarjetaPelicula.appendChild(descripcionPeli);
+    tarjetaPelicula.appendChild(imagenPeli);
+
+    seccionPeliculas.appendChild(tarjetaPelicula);
+});
+
+
+
+
