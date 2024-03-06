94255f375c3294db4881d69bf16bcbb7bd106d69
cfcamilafabre <camilafabre98@gmail.com>
2024-03-06 14:42:27 -0300

    actualizacion de scripts y hito 3 terminado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,67 +1,36 @@
-const seccionPeliculas = document.querySelector(".seccion-peliculas");
+const renderTarjetas = (title, year, director, duration, genre, rate, poster) => {
+    const seccionPeliculas = document.querySelector(".seccion-peliculas");
+    
+    const tarjetaPelicula = document.createElement('div');
+    tarjetaPelicula.classList.add("pelicula");
 
-class Movie {
-    constructor(title, year, director, duration, genre, rate, poster) {
-        this.title = title;
-        this.year = year;
-        this.director = director;
-        this.duration = duration;
-        this.genre = genre;
-        this.rate = rate;
-        this.poster = poster;
-    }
+    const tituloPeli = document.createElement('h3');
+    tituloPeli.classList.add("titulo-pelicula");
+    tituloPeli.innerHTML = title;
+
+    const descripcionPeli = document.createElement('p');
+    descripcionPeli.classList.add("descripcion-peli");
+    descripcionPeli.innerText = `Año: ${year}, 
+    Director: ${director}, duracion: ${duration}, genero: ${genre}, rate: ${rate}`;
+
+    const imagenPeli = document.createElement('img');
+    imagenPeli.classList.add("imagen-peli");
+    imagenPeli.src = poster;
+
+    tarjetaPelicula.appendChild(tituloPeli);
+    tarjetaPelicula.appendChild(descripcionPeli);
+    tarjetaPelicula.appendChild(imagenPeli);
+
+    seccionPeliculas.appendChild(tarjetaPelicula);
 }
 
-class Repository {
-    constructor() {
-        this.movies = [];
-    }
-
-    createMovie({ title, year, director, duration, genre, rate, poster }) {
-        const newMovie = new Movie(title, year, director, duration, genre, rate, poster);
-        this.movies.push(newMovie);
-    }
-}
-
-const repository = new Repository();
-
-const addMovie = () => {
-    $.get("https://students-api.2.us-1.fl0.io/movies", (movies) => {
-        movies.forEach((movie) => {
-            repository.createMovie(movie);
-        });
-        renderTarjetas();
-    });
-};
-
-const renderTarjetas = () => {
-    const movies = repository.movies;
-    movies.map((movie) => {
+$.get("https://students-api.2.us-1.fl0.io/movies", (movies) => {
+    movies.forEach((movie) => {
         const { title, year, director, duration, genre, rate, poster } = movie;
-
-        const tarjetaPelicula = document.createElement('div');
-        tarjetaPelicula.classList.add("pelicula");
-
-        const tituloPeli = document.createElement('h3');
-        tituloPeli.classList.add("titulo-pelicula");
-        tituloPeli.innerHTML = title;
-
-        const descripcionPeli = document.createElement('p');
-        descripcionPeli.classList.add("descripcion-peli");
-        descripcionPeli.innerText = `Año: ${year}, Director: ${director}, Duracion: ${duration}, Genero: ${genre}, Rate: ${rate}`;
-
-        const imagenPeli = document.createElement('img');
-        imagenPeli.classList.add("imagen-peli");
-        imagenPeli.src = poster;
-
-        tarjetaPelicula.appendChild(tituloPeli);
-        tarjetaPelicula.appendChild(descripcionPeli);
-        tarjetaPelicula.appendChild(imagenPeli);
-
-        seccionPeliculas.appendChild(tarjetaPelicula);
+        renderTarjetas(title, year, director, duration, genre, rate, poster);
     });
-};
+});
 
-addMovie();
+
 
 
