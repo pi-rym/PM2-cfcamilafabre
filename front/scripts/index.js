538e9b961551958d7152eff02cf713c84016f2cc
cfcamilafabre <camilafabre98@gmail.com>
2024-03-07 10:05:04 -0300

    hito 4 terminado. modularizacion del index.js, y optimizacion del codigo con chat gpt

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,36 +1,7 @@
-const renderTarjetas = (title, year, director, duration, genre, rate, poster) => {
-    const seccionPeliculas = document.querySelector(".seccion-peliculas");
-    
-    const tarjetaPelicula = document.createElement('div');
-    tarjetaPelicula.classList.add("pelicula");
+const renderTarjetas = require("./renderTarjetas");
 
-    const tituloPeli = document.createElement('h3');
-    tituloPeli.classList.add("titulo-pelicula");
-    tituloPeli.innerHTML = title;
-
-    const descripcionPeli = document.createElement('p');
-    descripcionPeli.classList.add("descripcion-peli");
-    descripcionPeli.innerText = `Año: ${year}, 
-    Director: ${director}, duracion: ${duration}, genero: ${genre}, rate: ${rate}`;
-
-    const imagenPeli = document.createElement('img');
-    imagenPeli.classList.add("imagen-peli");
-    imagenPeli.src = poster;
-
-    tarjetaPelicula.appendChild(tituloPeli);
-    tarjetaPelicula.appendChild(descripcionPeli);
-    tarjetaPelicula.appendChild(imagenPeli);
-
-    seccionPeliculas.appendChild(tarjetaPelicula);
-}
-
-$.get("https://students-api.2.us-1.fl0.io/movies", (movies) => {
-    movies.forEach((movie) => {
-        const { title, year, director, duration, genre, rate, poster } = movie;
-        renderTarjetas(title, year, director, duration, genre, rate, poster);
+$.get("https://students-api.2.us-1.fl0.io/movies" , (data) => {
+    data.forEach((movie) => {
+        renderTarjetas(movie);
     });
 });
-
-
-
-
---
+++ front/scripts/renderTarjetas.js
@@ -0,0 +1,28 @@
+const seccionPeliculas = document.querySelector(".seccion-peliculas");
+
+function renderTarjetas(movie) {
+    const { title, year, director, duration, genre, rate, poster } = movie;
+
+    const tarjetaPelicula = document.createElement('div');
+    tarjetaPelicula.classList.add("pelicula");
+
+    const tituloPeli = document.createElement('h3');
+    tituloPeli.classList.add("titulo-pelicula");
+    tituloPeli.innerHTML = title;
+
+    const descripcionPeli = document.createElement('p');
+    descripcionPeli.classList.add("descripcion-peli");
+    descripcionPeli.innerText = `Año: ${year}, Director: ${director}, Duración: ${duration}, Género: ${genre}, Rate: ${rate}`;
+
+    const imagenPeli = document.createElement('img');
+    imagenPeli.classList.add("imagen-peli");
+    imagenPeli.src = poster;
+
+    tarjetaPelicula.appendChild(tituloPeli);
+    tarjetaPelicula.appendChild(descripcionPeli);
+    tarjetaPelicula.appendChild(imagenPeli);
+
+    seccionPeliculas.appendChild(tarjetaPelicula);
+}
+
+module.exports = renderTarjetas;
