694101a2c16d94427e521320b3ff8eadbb198daa
cfcamilafabre <camilafabre98@gmail.com>
2024-03-12 09:30:14 -0300

    cambio de api

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,22 +1,16 @@
 const renderTarjetas = require("./renderTarjetas");
 const axios = require("axios");
 
-// $.get("https://students-api.2.us-1.fl0.io/movies" , (data) => {
-//     data.forEach((movie) => {
-//         renderTarjetas(movie);
-//     });
-// });
-
-
-const fetchMovies = async () => {
+const getMovies = async () => {
     try {
-        const response = await axios.get("https://students-api.2.us-1.fl0.io/movies");
+        const response = await axios.get("https://students-api.up.railway.app/movies")
         const movies = response.data;
         movies.forEach((movie) => {
             renderTarjetas(movie);
         });
     } catch (err) {
-        return err;
+        console.error(err);
     }
 }
 
+getMovies();
